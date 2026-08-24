dbce0153346159e863a66a20f7a09212c309a5b8
Easul <212490197@qq.com>
2026-08-24 18:36:32 +0800

    fix: align life runtime plugin build output

--- extensions/life-runtime/android/build.gradle.kts
+++ extensions/life-runtime/android/build.gradle.kts
@@ -3,6 +3,13 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    project.layout.buildDirectory.value(newBuildDir.dir(project.name))
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
